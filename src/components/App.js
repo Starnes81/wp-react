2395b889b549643a018cd56d57cd3c85884f3873
Steven Starnes <starnes81@gmail.com>
2017-11-10 12:01:42 -0600

    moved app into components folder

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <p className="App-intro">
+          WordPress React
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
